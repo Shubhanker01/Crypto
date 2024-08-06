b23ed12da72fad6873ad36a376d6f6d22e325697
Shubhanker01 <shubhanker40@gmail.com>
2024-08-06 17:54:57 +0530

    1st commit

---
+++ frontend/react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-3xl font-bold'>Today's Cryptocurrency prices by market cap</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
